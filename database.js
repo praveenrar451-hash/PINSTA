57f439e9c25c0d1a7d5d83bbb5a8b5277a085623
praveenrar451-hash <praveenrar451@gmail.com>
2026-08-26 16:46:50 +0530

    Implement message storage and retrieval functions
    Added functionality to save and retrieve messages between users.

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 let users = [];
 let posts = [];
+let messages = []; // Messages store karne ke liye array
 
 const db = {
   findUserByUsername: (username, callback) => {
@@ -41,7 +42,7 @@ const db = {
     const newPost = { 
       id: Date.now(), 
       ...postObj, 
-      likes: [], // Array of usernames who liked
+      likes: [], 
       comments: [],
       created_at: new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString() 
     };
@@ -55,9 +56,9 @@ const db = {
       if (!post.likes) post.likes = [];
       const index = post.likes.indexOf(username);
       if (index > -1) {
-        post.likes.splice(index, 1); // Unlike
+        post.likes.splice(index, 1);
       } else {
-        post.likes.push(username); // Like
+        post.likes.push(username);
       }
     }
     callback(null);
@@ -86,6 +87,27 @@ const db = {
       }
     }
     callback(null);
+  },
+
+  // Chat/Messages functions
+  saveMessage: (sender, receiver, text) => {
+    const msg = {
+      id: Date.now(),
+      sender,
+      receiver,
+      text,
+      timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+    };
+    messages.push(msg);
+    return msg;
+  },
+
+  getConversation: (user1, user2, callback) => {
+    const conv = messages.filter(m => 
+      (m.sender === user1 && m.receiver === user2) || 
+      (m.sender === user2 && m.receiver === user1)
+    );
+    callback(null, conv);
   }
 };
 
